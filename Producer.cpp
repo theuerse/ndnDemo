3a96a489d2e020a143bd981c2898946e9709436e
theuerse <theuers@posteo.de>
2015-12-09 16:03:39 +0100

    initial Commit, basic producer

---
+++ Producer.cpp
@@ -0,0 +1,147 @@
+#include "Producer.h"
+// public methods
+ndn::Producer::Producer(string prefix, int data_size, int freshness_seconds)
+{
+    this->prefix = prefix;
+    this-> data_size = data_size;
+    this->freshness_seconds = freshness_seconds;
+}
+
+ndn::Producer::~Producer()
+{
+    cout << "Bye you cruel world" << endl;
+}
+
+// register prefix on NFD
+void ndn::Producer::run()
+{
+    m_face.setInterestFilter(this->prefix,
+                             bind(&Producer::onInterest, this, _1, _2),
+                             RegisterPrefixSuccessCallback(),
+                             bind(&Producer::onRegisterFailed, this, _1, _2));
+    m_face.processEvents();
+}
+
+
+// private methods
+// generate random content of given length
+string ndn::Producer::generateContent(const int length)
+{
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    string content;
+
+    for (int i = 0; i < length; ++i)
+        content += alphanum[rand() % (sizeof(alphanum) - 1)];
+    return content;
+}
+
+// react to arrival of a Interest-Package
+void ndn::Producer::onInterest(const InterestFilter& filter, const Interest& interest)
+{
+    cout << "Received Interest: " << interest << endl;
+
+    // Create new name, based on Interest's name
+    Name dataName(interest.getName());
+    dataName.appendVersion();  // add "version" component (current UNIX timestamp in milliseconds)
+
+    string content = generateContent(data_size);
+
+    // Create Data packet
+    shared_ptr<Data> data = make_shared<Data>();
+    data->setName(dataName);
+    data->setFreshnessPeriod(time::seconds(freshness_seconds));
+    data->setContent(reinterpret_cast<const uint8_t*>(content.c_str()), content.size());
+
+    // Sign Data packet with default identity
+    m_keyChain.sign(*data);
+
+    // Return Data packet
+    m_face.put(*data);
+}
+
+// react to failure of prefix-registration
+void ndn::Producer::onRegisterFailed(const Name& prefix, const std::string& reason)
+{
+      cerr << "ERROR: Failed to register prefix \""
+                << prefix << "\" in local hub's daemon (" << reason << ")"
+                << endl;
+    m_face.shutdown();
+}
+
+//
+// Main entry-point
+//
+int main(int argc, char** argv)
+{
+  string appName = boost::filesystem::basename(argv[0]);
+
+  options_description desc("Programm Options");
+  desc.add_options ()
+      ("prefix,p", value<string>()->required (), "Prefix the Producer listens too. (Required)")
+      ("data-size,s", value<int>()->required (), "The size of the datapacket in bytes. (Required)")
+      ("freshness-time,f", value<int>(), "Freshness time of the content in seconds. (Default 5min)");
+
+  positional_options_description positionalOptions;
+  variables_map vm;
+
+  try
+  {
+    store(command_line_parser(argc, argv).options(desc)
+                .positional(positionalOptions).run(),
+              vm); // throws on error
+
+    notify(vm); //notify if required parameters are not provided.
+  }
+  catch(boost::program_options::required_option& e)
+  {
+    // user forgot to provide a required option
+    cerr << "prefix         ... Prefix the Producer listens to. (Required)" << endl;
+    cerr << "data-size      ... The size of the datapacket in bytes  (Required)" << endl;
+    cerr << "freshness-time ... Freshness time of the content in seconds (Default 5 min)" << endl;
+    cerr << "usage-example: " << "./" << appName << " --prefix foo --data-size 12" << endl;
+    cerr << "usage-example: " << "./" << appName << " --prefix foo --data-size 12 --freshness-time 23" << endl;
+
+    cerr << "ERROR: " << e.what() << endl << endl;
+    return -1;
+  }
+  catch(boost::program_options::error& e)
+  {
+    // a given parameter is faulty (e.g. given a string, asked for int)
+    cerr << "ERROR: " << e.what() << endl << endl;
+    return -1;
+  }
+  catch(exception& e)
+  {
+    cerr << "Unhandled Exception reached the top of main: "
+              << e.what() << ", application will now exit" << endl;
+    return -1;
+  }
+
+  int freshness_time = 300;
+  if(vm.count ("freshness-time"))
+  {
+    freshness_time = vm["freshness-time"].as<int>();
+  }
+
+  // create new Producer instance with given parameters
+  ndn::Producer producer(vm["prefix"].as<string>(),
+                         vm["data-size"].as<int>(),
+                         freshness_time);
+
+  try
+  {
+    // start producer
+    producer.run();
+  }
+  catch (const exception& e)
+  {
+    // shit happens
+    cerr << "ERROR: " << e.what() << endl;
+  }
+
+  return 0;
+}
---
+++ Producer.h
@@ -0,0 +1,39 @@
+// ndn-File-Producer
+// works like a simple http-server
+
+#ifndef PRODUCER_H
+#define PRODUCER_H
+
+// for communication with NFD
+#include <ndn-cxx/face.hpp>
+#include <ndn-cxx/security/key-chain.hpp>
+
+// using boost for file-system handling
+#include "boost/program_options.hpp"
+#include "boost/filesystem.hpp"
+
+using namespace std;
+using namespace boost::program_options;
+
+namespace ndn {
+class Producer
+{
+    public:
+        Producer(string prefix, int data_size, int freshness_seconds);
+        void run();
+        virtual ~Producer();
+    protected:
+    private:
+        string generateContent(const int length);
+        void onInterest(const InterestFilter& filter, const Interest& interest);
+        void onRegisterFailed(const Name& prefix, const std::string& reason);
+
+        Face m_face;
+        KeyChain m_keyChain;
+        string prefix;
+        int data_size;
+        int freshness_seconds;
+};
+}   // end namespace ndn
+
+#endif // PRODUCER_H
